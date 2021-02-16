78941439380d343b31054844065d160dfddfec76
chirag agrawal <cheeruagrawal@gmail.com>
2021-02-16 21:17:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,331 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import logo from "./logo.svg";
+// import './App.css';
+import background from "./assets/images/background.jpg";
+import img1 from "./assets/images/img1.jpg";
+import { Card, Col, Row } from "react-bootstrap";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { Button, Input } from "reactstrap";
+import React, { Component } from "react";
+// import { Button } from "bootstrap";
+import axios from "axios";
+import moment from "moment";
+import TimePicker from "react-time-picker";
+import "./App.css";
+var date = new Date();
+export default class App extends Component {
+  state = {
+    searchValue: "",
+    week: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    restArray: [],
+    date: date,
+    dataUpdate: false,
+    today: null,
+    currentTime: null,
+    selectedDay: null,
+    selectedTime: null,
+    search: null,
+    searching: false,
+    showCalendar: false,
+    showTimePicker: false,
+  };
+  componentDidMount() {
+    axios
+      .get("https://run.mocky.io/v3/b0f3e975-b815-4e88-8a6a-84af59fe32eb")
+      .then((data) => {
+        const dataArray = data.data;
+        const restArray = [];
+        dataArray.forEach((ele, index) => {
+          const timeString = ele["Mon-Sun 11:30 am - 9 pm"];
+          const timeobject = this.handleTimeString(timeString, index);
+          console.log(timeobject);
+          const newEle = {
+            restaurant: ele["Kushi Tsuru"],
+            start: timeobject.startArray,
+            end: timeobject.endArray,
+          };
+          restArray.push(newEle);
+        });
+        this.setState({
+          restArray,
+          date,
+          today: date.getDay(),
+          selectedDay: date.getDay(),
+          currentTime: date.getTime(),
+          selectedTime: date.getTime(),
+          dataUpdate: true,
+        });
+        // console.log(dataArray);
+      });
+    // console.log(restaurants);
+  }
+  handleTimeString = (string, index) => {
+    const startArray = [];
+    const endArray = [];
+    if (index == 33) {
+      for (
+        let i = this.state.week.indexOf(string.slice(5, 8));
+        i <= this.state.week.indexOf(string.slice(9, 12));
+        i++
+      ) {
+        startArray[i] = string.slice(13).split(" - ")[0];
+        endArray[i] = string.slice(13).split(" - ")[1];
+      }
+      let j = this.state.week.indexOf(string.slice(0, 3));
+      startArray[j] = string.slice(13).split(" - ")[0];
+      endArray[j] = string.slice(13).split(" - ")[1];
+    } else {
+      if (string.includes("/")) {
+        string.split("  / ").forEach((ind) => {
+          if (ind[3] == "-") {
+            if (ind.includes(",")) {
+              for (
+                let i = this.state.week.indexOf(ind.slice(0, 3));
+                i <= this.state.week.indexOf(ind.slice(4, 7));
+                i++
+              ) {
+                startArray[i] = ind.slice(13).split(" - ")[0];
+                endArray[i] = ind.slice(13).split(" - ")[1];
+              }
+              let j = this.state.week.indexOf(ind.slice(9, 12));
+              startArray[j] = ind.slice(13).split(" - ")[0];
+              endArray[j] = ind.slice(13).split(" - ")[1];
+            } else {
+              for (
+                let i = this.state.week.indexOf(ind.slice(0, 3));
+                i <= this.state.week.indexOf(ind.slice(4, 7));
+                i++
+              ) {
+                startArray[i] = ind.slice(8).split(" - ")[0];
+                endArray[i] = ind.slice(8).split(" - ")[1];
+              }
+            }
+          } else {
+            let k = this.state.week.indexOf(ind.slice(0, 3));
+            startArray[k] = ind.slice(4).split(" - ")[0];
+            endArray[k] = ind.slice(4).split(" - ")[1];
+          }
+        });
+      } else {
+        for (let i = 0; i < 7; i++) {
+          startArray.push(string.slice(8).split(" - ")[0]);
+          endArray.push(string.slice(8).split(" - ")[1]);
+        }
+      }
+    }
+    // console.log(startArray, endArray, string);
+    return {
+      startArray: startArray,
+      endArray: endArray,
+    };
+  };
+  validRest = (object) => {
+    const begin = moment(object.start[this.state.selectedDay], "h:mma");
+    const end = moment(object.end[this.state.selectedDay], "h:mma");
+    const time = moment(this.state.selectedTime);
+    console.log(time.isBetween(begin, end), begin, end, time);
+    return time.isBetween(begin, end);
+  };
+  isSearched = (res, string) => {
+    if (string.length > 0) {
+      if (res.restaurant.toLowerCase().includes(string.toLowerCase())) {
+        return true;
+      } else return false;
+    } else {
+      this.setState({ searching: false });
+    }
+  };
+  handleRestInput = (event) => {
+    this.setState({ [event.target.name]: event.target.value, searching: true });
+  };
+  handleDate = (value) => {
+    date = new Date(value);
+    this.setState(
+      {
+        date,
+        selectedDay: date.getDay(),
+        showCalendar: false,
+      },
+      () => {
+        console.log(this.state.selectedDay);
+      }
+    );
+  };
+  handleTime = (value) => {
+    // this.setState;
+    console.log(value);
+  };
+  comparator = (a, b) => {
+    const closeA = moment(a.end[this.state.selectedDay], "hh:mma");
+    const closeB = moment(b.end[this.state.selectedDay], "hh:mma");
+    if (closeA.isBefore(closeB)) {
+      return -1;
+    } else {
+      return 1;
+    }
+  };
+  handleSort = () => {
+    const arr = this.state.restArray;
+    arr.sort(this.comparator);
+    this.setState({ restArray: arr });
+  };
+  // handleOneRest=(res)=>{
+  //   this.setState({selectedRest:res})
+  // }
+  render() {
+    return (
+      <div className="App">
+        <div
+          className="mb-5"
+          style={{
+            backgroundImage: "url(" + background + ")",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "65vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexFlow: "column",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="my-3">
+            <h1 style={{ fontWeight: "bolder", color: "white" }}>FindFood</h1>
+          </div>
+          <Row className="w-75">
+            <Col md={2}>
+              <Button
+                style={{
+                  color: "black",
+                  backgroundColor: "white",
+                  fontSize: "1.4rem",
+                  width: "100%",
+                }}
+                onClick={() => {
+                  this.setState({ showTimePicker: !this.state.showTimePicker });
+                }}
+              >
+                {moment(this.state.date.getTime()).format("hh:mma")}
+              </Button>
+              {this.state.showTimePicker && (
+                <TimePicker
+                  className="time-class"
+                  disableClock="true"
+                  onChange={this.handleTime}
+                />
+              )}
+            </Col>
+            <Col md={2}>
+              <Button
+                style={{
+                  color: "black",
+                  backgroundColor: "white",
+                  fontSize: "1.4rem",
+                  width: "100%",
+                }}
+                onClick={() => {
+                  this.setState({ showCalendar: !this.state.showCalendar });
+                }}
+              >
+                {this.state.date.getDate() +
+                  "/" +
+                  this.state.date.getMonth() +
+                  "/" +
+                  this.state.date.getFullYear()}
+              </Button>
+              {this.state.showCalendar && (
+                <Calendar
+                  className="calendar-class"
+                  onChange={this.handleDate}
+                />
+              )}
+            </Col>
+            <Col>
+              <Input
+                className="py-4"
+                placeholder="Search for a restaurant"
+                name="search"
+                value={this.state.search}
+                onChange={this.handleRestInput}
+              />
+              {this.state.searching && (
+                <div
+                  className="py-2 mt-1 shadow"
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: "10px",
+                    maxHeight: "500px",
+                    overflow: "auto",
+                    position: "absolute",
+                    zIndex: 1000,
+                  }}
+                >
+                  {this.state.restArray.map(
+                    (res) =>
+                      this.isSearched(res, this.state.search.trim()) && (
+                        <Row className="my-3">
+                          <Col sm={"auto"}>
+                            <img
+                              height="60px"
+                              width="60px"
+                              src={img1}
+                              style={{ borderRadius: "10px" }}
+                            />
+                          </Col>
+                          <Col>
+                            <div>{res.restaurant}</div>
+                            <small>
+                              {res.end[this.state.selectedDay]
+                                ? "Open till " + res.end[this.state.selectedDay]
+                                : "Closed"}
+                            </small>
+                          </Col>
+                        </Row>
+                      )
+                  )}
+                </div>
+              )}
+            </Col>
+          </Row>
+        </div>
+        {this.state.dataUpdate && (
+          <div className="text-center">
+            <Row>
+              <Col>
+                <Button onClick={this.handleSort}>Sort by Closest time</Button>
+              </Col>
+            </Row>
+            <Row className="mx-auto" style={{ width: "80%" }}>
+              {this.state.restArray.map(
+                (res) =>
+                  res.start[this.state.selectedDay] &&
+                  this.validRest(res) && (
+                    <Col md={3}>
+                      <Card
+                        className="shadow my-4"
+                        style={{
+                          borderRadius: "15px",
+                          fontSize: "1rem",
+                          overflow: "hidden",
+                          width: "100%",
+                        }}
+                      >
+                        <img height="100%" width="100%" src={img1} />
+                        <div className="p-2">
+                          <div>{res.restaurant}</div>
+                          <small>
+                            {"Open till " + res.end[this.state.selectedDay]}
+                          </small>
+                        </div>
+                      </Card>
+                    </Col>
+                  )
+              )}
+            </Row>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,206 @@
+module.exports.restaurants = [
+  {
+    restaurant: "Osakaya Restaurant",
+    timing: "Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 9:30 pm",
+  },
+  {
+    restaurant: "The Stinking Rose",
+    timing: "Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 11 pm",
+  },
+  {
+    restaurant: "McCormick & Kuleto's",
+    timing: "Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 11 pm",
+  },
+  {
+    restaurant: "Mifune Restaurant",
+    timing: "Mon-Sun 11 am - 10 pm",
+  },
+  {
+    restaurant: "The Cheesecake Factory",
+    timing:
+      "Mon-Thu 11 am - 11 pm  / Fri-Sat 11 am - 12:30 am  / Sun 10 am - 11 pm",
+  },
+  {
+    restaurant: "New Delhi Indian Restaurant",
+    timing: "Mon-Sat 11:30 am - 10 pm  / Sun 5:30 pm - 10 pm",
+  },
+  {
+    restaurant: "Iroha Restaurant",
+    timing: "Mon-Thu, Sun 11:30 am - 9:30 pm  / Fri-Sat 11:30 am - 10 pm",
+  },
+  {
+    restaurant: "Rose Pistola",
+    timing: "Mon-Thu 11:30 am - 10 pm  / Fri-Sun 11:30 am - 11 pm",
+  },
+  {
+    restaurant: "Alioto's Restaurant",
+    timing: "Mon-Sun 11 am - 11 pm",
+  },
+  {
+    restaurant: "Canton Seafood & Dim Sum Restaurant",
+    timing: "Mon-Fri 10:30 am - 9:30 pm  / Sat-Sun 10 am - 9:30 pm",
+  },
+  {
+    restaurant: "All Season Restaurant",
+    timing: "Mon-Fri 10 am - 9:30 pm  / Sat-Sun 9:30 am - 9:30 pm",
+  },
+  {
+    restaurant: "Bombay Indian Restaurant",
+    timing: "Mon-Sun 11:30 am - 10:30 pm",
+  },
+  {
+    restaurant: "Sam's Grill & Seafood Restaurant",
+    timing: "Mon-Fri 11 am - 9 pm  / Sat 5 pm - 9 pm",
+  },
+  {
+    restaurant: "2G Japanese Brasserie",
+    timing: "Mon-Thu, Sun 11 am - 10 pm  / Fri-Sat 11 am - 11 pm",
+  },
+  {
+    restaurant: "Restaurant Lulu",
+    timing: "Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 10 pm",
+  },
+  {
+    restaurant: "Sudachi",
+    timing:
+      "Mon-Wed 5 pm - 12:30 am  / Thu-Fri 5 pm - 1:30 am  / Sat 3 pm - 1:30 am  / Sun 3 pm - 11:30 pm",
+  },
+  {
+    restaurant: "Hanuri",
+    timing: "Mon-Sun 11 am - 12 am",
+  },
+  {
+    restaurant: "Herbivore",
+    timing: "Mon-Thu, Sun 9 am - 10 pm  / Fri-Sat 9 am - 11 pm",
+  },
+  {
+    restaurant: "Penang Garden",
+    timing:
+      "Mon-Thu 11 am - 10 pm  / Fri-Sat 10 am - 10:30 pm  / Sun 11 am - 11 pm",
+  },
+  {
+    restaurant: "John's Grill",
+    timing: "Mon-Sat 11 am - 10 pm  / Sun 12 pm - 10 pm",
+  },
+  {
+    restaurant: "Quan Bac",
+    timing: "Mon-Sun 11 am - 10 pm",
+  },
+  {
+    restaurant: "Bamboo Restaurant",
+    timing: "Mon-Sat 11 am - 12 am  / Sun 12 pm - 12 am",
+  },
+  {
+    restaurant: "Burger Bar",
+    timing: "Mon-Thu, Sun 11 am - 10 pm  / Fri-Sat 11 am - 12 am",
+  },
+  {
+    restaurant: "Blu Restaurant",
+    timing: "Mon-Fri 11:30 am - 10 pm  / Sat-Sun 7 am - 3 pm",
+  },
+  {
+    restaurant: "Naan 'N' Curry",
+    timing: "Mon-Sun 11 am - 4 am",
+  },
+  {
+    restaurant: "Shanghai China Restaurant",
+    timing: "Mon-Sun 11 am - 9:30 pm",
+  },
+  {
+    restaurant: "Tres",
+    timing: "Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 11 pm",
+  },
+  {
+    restaurant: "Isobune Sushi",
+    timing: "Mon-Sun 11:30 am - 9:30 pm",
+  },
+  {
+    restaurant: "Viva Pizza Restaurant",
+    timing: "Mon-Sun 11 am - 12 am",
+  },
+  {
+    restaurant: "Far East Cafe",
+    timing: "Mon-Sun 11:30 am - 10 pm",
+  },
+  {
+    restaurant: "Parallel 37",
+    timing: "Mon-Sun 11:30 am - 10 pm",
+  },
+  {
+    restaurant: "Bai Thong Thai Cuisine",
+    timing: "Mon-Sat 11 am - 11 pm  / Sun 11 am - 10 pm",
+  },
+  {
+    restaurant: "Alhamra",
+    timing: "Mon-Sun 11 am - 11 pm",
+  },
+  {
+    restaurant: "A-1 Cafe Restaurant",
+    timing: "Mon, Wed-Sun 11 am - 10 pm",
+  },
+  {
+    restaurant: "Nick's Lighthouse",
+    timing: "Mon-Sun 11 am - 10:30 pm",
+  },
+  {
+    restaurant: "Paragon Restaurant & Bar",
+    timing: "Mon-Fri 11:30 am - 10 pm  / Sat 5:30 pm - 10 pm",
+  },
+  {
+    restaurant: "Chili Lemon Garlic",
+    timing: "Mon-Fri 11 am - 10 pm  / Sat-Sun 5 pm - 10 pm",
+  },
+  {
+    restaurant: "Bow Hon Restaurant",
+    timing: "Mon-Sun 11 am - 10:30 pm",
+  },
+  {
+    restaurant: "San Dong House",
+    timing: "Mon-Sun 11 am - 11 pm",
+  },
+  {
+    restaurant: "Thai Stick Restaurant",
+    timing: "Mon-Sun 11 am - 1 am",
+  },
+  {
+    restaurant: "Cesario's",
+    timing: "Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 10:30 pm",
+  },
+  {
+    restaurant: "Colombini Italian Cafe Bistro",
+    timing: "Mon-Fri 12 pm - 10 pm  / Sat-Sun 5 pm - 10 pm",
+  },
+  {
+    restaurant: "Sabella & La Torre",
+    timing: "Mon-Thu, Sun 10 am - 10:30 pm  / Fri-Sat 10 am - 12:30 am",
+  },
+  {
+    restaurant: "Soluna Cafe and Lounge",
+    timing: "Mon-Fri 11:30 am - 10 pm  / Sat 5 pm - 10 pm",
+  },
+  {
+    restaurant: "Tong Palace",
+    timing: "Mon-Fri 9 am - 9:30 pm  / Sat-Sun 9 am - 10 pm",
+  },
+  {
+    restaurant: "India Garden Restaurant",
+    timing: "Mon-Sun 10 am - 11 pm",
+  },
+  {
+    restaurant: "Sapporo-Ya Japanese Restaurant",
+    timing: "Mon-Sat 11 am - 11 pm  / Sun 11 am - 10:30 pm",
+  },
+  {
+    restaurant: "Santorini's Mediterranean Cuisine",
+    timing: "Mon-Sun 8 am - 10:30 pm",
+  },
+  {
+    restaurant: "Kyoto Sushi",
+    timing:
+      "Mon-Thu 11 am - 10:30 pm  / Fri 11 am - 11 pm  / Sat 11:30 am - 11 pm  / Sun 4:30 pm - 10:30 pm",
+  },
+  {
+    restaurant: "Marrakech Moroccan Restaurant",
+    timing: "Mon-Sun 5:30 pm - 2 am",
+  },
+];
